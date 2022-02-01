88b7fc0ae338384c3f166cf8159f6bdf851e6089
Michael Harty <michaelharty3@gmail.com>
2022-01-31 22:09:31 -0700

    fix suggesting repeat yellow in the same spot and add some error checking

--- app.py
+++ app.py
@@ -1,48 +1,63 @@
 from numpy import can_cast
 import streamlit as st
 
-st.title('Wordle')
-
-guess1 = st.text_input('what did u guess')
-
-response1 = st.text_input('what did it say b for black, y for yellow or g for green')
-
-emoji_map = {'b': '⬛', 'y': '🟨', 'g': '🟩'}
-table = str.maketrans(emoji_map)
-st.write(response1.translate(table))
-
 
 def parse_word(guess, response):
     cant_contain = []
-    does_contain = []
-    locked_in = {}
+    wrong_spot = {}
+    right_spot = {}
     for i, (c, r) in enumerate(zip(guess, response)):
         if r == 'b':
             cant_contain.append(c)
+
         if r == 'y':
-            does_contain.append(c)
+            if c in wrong_spot:
+                wrong_spot[c].append(i)
+            else:
+                wrong_spot[c] = [i]
+
         if r == 'g':
-            locked_in[i] = c
-    return cant_contain, does_contain, locked_in
-
-
+            right_spot[i] = c
+    return cant_contain, wrong_spot, right_spot
+    
 
 def filter_words(wordlist, rules):
     cant_contain = rules[0]
-    does_contain = rules[1]
-    locked_in = rules[2]
+    wrong_spot = rules[1]
+    right_spot = rules[2]
+    # filter out blacks
     wordlist = [word for word in wordlist if not any(char in word for char in cant_contain)]
-    if does_contain:
-        wordlist = [word for word in wordlist if any(char in word for char in does_contain)]
     
-    for idx, char in locked_in.items():
+    for char, indexes in wrong_spot.items():
+        for i in indexes:
+            wordlist = [w for w in wordlist if char in w and list(w)[i] != char]
+    
+    for idx, char in right_spot.items():
         print(idx, char)
         wordlist = [word for word in wordlist if list(word)[idx] == char]
     
     return wordlist
 
 
-wordlist = open('wordlist.txt').readlines()
+
+st.title('Wordle')
+guess1 = st.text_input('what was your first guess')
+if guess1:
+    assert len(guess1) == 5
+
+response1 = st.text_input('what did it say b for black, y for yellow or g for green')
+if response1:
+    assert len(response1) == 5
+    assert set(response1).issubset(set('byg'))
+
+emoji_map = {'b': '⬛', 'y': '🟨', 'g': '🟩'}
+table = str.maketrans(emoji_map)
+st.write(response1.translate(table))
+
+
+wordlist = open('sorted_words.txt').readlines()
 rules = parse_word(guess1, response1)
-st.write(rules)
-st.write(filter_words(wordlist, rules))
+if response1:
+    st.write('## Some good next guesses:')
+    for word in filter_words(wordlist, rules)[:5]:
+        st.write(word)
